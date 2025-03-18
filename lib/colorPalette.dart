2bda93aa73225d5b7b491244ceb208c559589702
DerFl3x <d3rflex@proton.me>
2025-03-18 15:50:46 +0100

    - removed default file "widget_test.dart" - added bottom snack bar (settings, search bar, add 2fa/camera) without functionality - added a simple color palette

---
+++ lib/colorPalette.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ColorPalette {
+  static const Color primaryColor = Color(0xFFBB00FF); // Blue-Purple
+  static const Color darkAccentColor = Color(0xFF8A00CC); // Darker Blue-Purple
+  static const Color lightAccentColor = Color(0xFFD133FF); // Light Blue-Purple
+  static const Color backgroundColor = Color(0xFFF1E0FF); // Light Purple
+  static const Color textColor = Color(0xFFFFFFFF); // White
+  static const Color secondaryColor = Color(0xFFA566FF); // Muted Blue-Purple
+}
